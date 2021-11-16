36d56edefd5203c33d399e293d7e8971413e9c06
Ericzklm <52121058+Ericzklm@users.noreply.github.com>
2021-11-16 02:49:21 -0800

    Added balloons and shooting basics

---
+++ Unity/Assets/Shooter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+    public float damage = 100f;
+    public float range = Mathf.Infinity;
+    public int layerMask = 0;
+
+    public Camera fpsCam;
+
+    // Start is called before the first frame update
+    void Start() {
+        layerMask = 1 << 9;
+        layerMask = ~layerMask;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetButtonDown("Fire1")) {
+            Shoot();
+        }
+    }
+
+    void Shoot() {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range, layerMask)) {
+            Debug.Log(hit.transform.name); 
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null) {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
---
+++ Unity/Assets/WebAPIAccess.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class WebAPIAccess : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    IEnumerator Upload() {
+        WWWForm form = new WWWForm();
+        form.AddField("username", "Sparrow");
+        form.AddField("score", 41);
+        form.AddField("balloons_popped", 14);
+        form.AddField("shot_accuracy", ".3");
+        form.AddField("boosts_used", 3);
+
+        using (UnityWebRequest www = UnityWebRequest.Post("https://aeroplay.herokuapp.com/api/insert", form)) {
+            yield return www.SendWebRequest();
+            if (www.isNetworkError) {
+                Debug.Log(www.error);
+            }
+            else {
+                Debug.Log("Success! " + www.downloadHandler.text);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown("m")) {
+            StartCoroutine(Upload());
+            //Upload();
+        }
+    }
+}
