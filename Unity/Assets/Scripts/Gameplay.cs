66e5cf849b569e930c39c5130e2ea0f86bfd23c8
kevinwiranata <kevin.a.wiranata@gmail.com>
2021-11-22 21:54:41 -0800

    initial gcp

---
+++ Unity/Assets/Scripts/Gameplay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class Gameplay
+{
+	public static bool isPaused = false;
+
+	public static void pauseGame()
+	{
+		if (!isPaused)
+		{
+			Debug.Log("pause");
+			SceneManager.LoadScene("Menu Scene");
+			isPaused = !isPaused;
+		}
+	}
+
+	public static void resumeGame()
+	{
+		if (isPaused)
+		{
+			Debug.Log("resume");
+			SceneManager.LoadScene("Main Scene");
+			isPaused = !isPaused;
+		}
+	}
+
+	public static void quitGame()
+	{
+		Application.Quit();
+	}
+}
