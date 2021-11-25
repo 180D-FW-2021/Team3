29d5814c18f49e9420f908ed6885864eefcce64a
Leeksun Cho <leeksuncho@Leeksuns-MacBook-Pro.local>
2021-11-24 18:12:48 -0800

    Made a new object in Unity to display the IP Address - still have problems as the script will not run

---
+++ Unity/Assets/Scripts/IPAddress.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Linq;
+using UnityEngine;
+
+public class IPAddress : MonoBehaviour
+{
+	public string output = "0.0.0.0";
+	public Text myText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+    	IPAddress ipAddr = ipHost.AddressList.FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork);
+    	output = ipAddr.MapToIPv4().ToString();
+    	myText.text = output;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
