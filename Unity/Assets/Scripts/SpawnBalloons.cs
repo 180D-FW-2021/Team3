1fe31205eec5730f7caccd5675c76ca9cb0049dd
Ericzklm <52121058+Ericzklm@users.noreply.github.com>
2021-12-09 19:07:14 -0800

    Added balloon movement

--- Unity/Assets/Scripts/SpawnBalloons.cs
+++ Unity/Assets/Scripts/SpawnBalloons.cs
@@ -12,7 +12,11 @@ public class SpawnBalloons : MonoBehaviour {
     public GameObject BalloonPreFab5;
     public GameObject BalloonPreFab10;
 
+    public static Vector3 wind;
+
     void Start() {
+        wind = new Vector3(Random.Range(-.3f, .3f), Random.Range(-.1f, .1f), Random.Range(-.3f, .3f));
+
         for (int balloonNum = 0; balloonNum < 100; balloonNum++) {
             Instantiate(BalloonPreFab1, new Vector3(Random.Range(-3000,3000),Random.Range(47,1000),Random.Range(-3000,3000)), Quaternion.identity);
         }
@@ -28,10 +32,5 @@ public class SpawnBalloons : MonoBehaviour {
         for (int balloonNum = 0; balloonNum < 5; balloonNum++) {
             Instantiate(BalloonPreFab10, new Vector3(Random.Range(-3000,3000),Random.Range(47,1000),Random.Range(-3000,3000)), Quaternion.identity);
         }
-    }
-
-    // Update is called once per frame
-    void Update() {
-        
     }
 }
--- Unity/Assets/Scripts/Target.cs
+++ Unity/Assets/Scripts/Target.cs
@@ -1,15 +1,53 @@
 ﻿using UnityEngine;
+using System;
 
 public class Target : MonoBehaviour
 {
+    public float index = 0f;
     public float health = 1f;
-    public void TakeDamage(float amount) {
+    public float deltaHeight = .5f;
+    public void TakeDamage(float amount) 
+    {
         health -= amount;
-        if (health <= 0f) {
+        if (health <= 0f) 
+        {
             Die();
         }
     }
-    void Die() {
+
+    void Die() 
+    {
         Destroy(gameObject);
+    }
+
+    public void Start()
+    {
+        index = UnityEngine.Random.Range(0f, 6.28f);
+    }
+
+    public float GetWindModifier()
+    {
+        switch(this.name)
+        {
+            case "Balloon1(Clone)":
+                return 0f;
+            case "Balloon2(Clone)":
+                return .1f;
+            case "Balloon3(Clone)":
+                return .3f;
+            case "Balloon5(Clone)":
+                return .5f;
+            case "Balloon10(Clone)":
+                return 1f;
+            default:
+                return 0f;
+        }
+    }
+
+    public void Update() 
+    {
+        index += Time.deltaTime;
+        float y = deltaHeight * Mathf.Sin(index);
+        transform.localPosition = this.transform.position + new Vector3(0,y,0) + SpawnBalloons.wind * GetWindModifier();
     }
 }
