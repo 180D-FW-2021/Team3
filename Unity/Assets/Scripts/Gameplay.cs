398c90a9cc40496ebcb47ddce36d5ad59a184a80
kevinwiranata <kevin.a.wiranata@gmail.com>
2022-01-09 12:57:37 +0700

    fixed keyboard mode

--- Unity/Assets/Scripts/Gameplay.cs
+++ Unity/Assets/Scripts/Gameplay.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public static class Gameplay 
+public static class Gameplay
 {
 	[SerializeField] static GameObject MainMenuContainer;
 	[SerializeField] static GameObject OptionsMenu;
