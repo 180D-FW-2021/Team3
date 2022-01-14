be317fd117b55e0633a5dac7a5f3f5fe48308783
kevinwiranata <kevin.a.wiranata@gmail.com>
2022-01-14 17:22:19 +0700

    smoothen camera blending

--- Unity/Assets/Scripts/CameraControl.cs
+++ Unity/Assets/Scripts/CameraControl.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    [SerializeField] float bias = 0.95f; 
+    [SerializeField] float bias = 0.971f; 
 	PlaneControl planeControl;
 
 	// Start is called before the first frame update
